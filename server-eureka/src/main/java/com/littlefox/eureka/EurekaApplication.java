d2fbf40bf678fd4d0540b076a2a959edae14e844
rockychen <rockychen1221@163.com>
2019-10-31 19:51:01 +0800

    Fix Eureka

--- server-eureka/src/main/java/com/littlefox/eureka/EurekaApplication.java
+++ server-eureka/src/main/java/com/littlefox/eureka/EurekaApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableEurekaServer
+@EnableEurekaServer //开启Eureka Server
 public class EurekaApplication {
     public static void main(String[] args) {
         SpringApplication.run(EurekaApplication.class,args);
